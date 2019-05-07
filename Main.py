1580c97374a1510209a39cfb6828f771bc279876
JohnyHCL <44863546+JohnyHCL@users.noreply.github.com>
2019-05-07 05:14:23 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -12,14 +12,18 @@ from PIL import Image
 from scipy.ndimage import imread
 from PIL.ImageQt import ImageQt
 import pyqtgraph as pg
+import openpyxl as xl
 import requests
 import json
 import time
+import threading
+
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(749, 733)
+        MainWindow.setStyleSheet("")
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
@@ -31,7 +35,7 @@ class Ui_MainWindow(object):
         self.scrollArea_2.setWidgetResizable(True)
         self.scrollArea_2.setObjectName("scrollArea_2")
         self.hForecastSA2 = QtWidgets.QWidget()
-        self.hForecastSA2.setGeometry(QtCore.QRect(0, 0, 727, 292))
+        self.hForecastSA2.setGeometry(QtCore.QRect(0, 0, 727, 276))
         self.hForecastSA2.setObjectName("hForecastSA2")
         self.gridLayout_2 = QtWidgets.QGridLayout(self.hForecastSA2)
         self.gridLayout_2.setObjectName("gridLayout_2")
@@ -46,7 +50,7 @@ class Ui_MainWindow(object):
         self.scrollArea_1.setWidgetResizable(True)
         self.scrollArea_1.setObjectName("scrollArea_1")
         self.hForecastSA1 = QtWidgets.QWidget()
-        self.hForecastSA1.setGeometry(QtCore.QRect(0, 0, 727, 292))
+        self.hForecastSA1.setGeometry(QtCore.QRect(0, 0, 727, 277))
         self.hForecastSA1.setObjectName("hForecastSA1")
         self.gridLayout_4 = QtWidgets.QGridLayout(self.hForecastSA1)
         self.gridLayout_4.setObjectName("gridLayout_4")
@@ -55,7 +59,7 @@ class Ui_MainWindow(object):
         self.gridLayout_4.addLayout(self.scrollArea_1_layout, 0, 0, 1, 1)
         self.scrollArea_1.setWidget(self.hForecastSA1)
         self.gridFor2.addWidget(self.scrollArea_1, 0, 0, 1, 1)
-        self.gridLayout.addLayout(self.gridFor2, 2, 0, 1, 1)
+        self.gridLayout.addLayout(self.gridFor2, 3, 0, 1, 1)
         self.gridLayout_3 = QtWidgets.QGridLayout()
         self.gridLayout_3.setObjectName("gridLayout_3")
         spacerItem = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
@@ -120,7 +124,16 @@ class Ui_MainWindow(object):
         self.rain.setAlignment(QtCore.Qt.AlignCenter)
         self.rain.setObjectName("rain")
         self.gridLayout_3.addWidget(self.rain, 2, 7, 1, 1)
-        self.gridLayout.addLayout(self.gridLayout_3, 0, 0, 1, 1)
+        self.gridLayout.addLayout(self.gridLayout_3, 1, 0, 1, 1)
+        self.search_grid = QtWidgets.QGridLayout()
+        self.search_grid.setObjectName("search_grid")
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setObjectName("lineEdit")
+        self.search_grid.addWidget(self.lineEdit, 0, 0, 1, 1)
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setObjectName("pushButton")
+        self.search_grid.addWidget(self.pushButton, 0, 1, 1, 1)
+        self.gridLayout.addLayout(self.search_grid, 0, 0, 1, 1)
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 749, 21))
@@ -150,12 +163,16 @@ class Ui_MainWindow(object):
         self.wind.setText(_translate("MainWindow", "wind"))
         self.winddir.setText(_translate("MainWindow", "winddir"))
         self.rain.setText(_translate("MainWindow", "rain"))
+        self.pushButton.setText(_translate("MainWindow", "Get Forecast"))
 
-    def set_background(self):
+    def set_misc(self):
+        
         self.retranslateUi(MainWindow)
+        self.pushButton.clicked.connect(self.refresh)
+        self.pushButton.setShortcut("Return")
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
         oImage = QImage("icons\\background.png")
-        sImage = oImage.scaled(QSize(749,733))                   
+        sImage = oImage.scaled(QSize(749,733))
         palette = QPalette()
         palette.setBrush(10, QBrush(oImage))                     
         MainWindow.setPalette(palette)
@@ -186,21 +203,65 @@ class Ui_MainWindow(object):
                 }
         
         """
-         
+        
+        lineEditStyleSheet = """
+        
+            *{
+                background-color: rgba(0, 0, 0, 50);
+                color: white;
+            }
+        
+        """
+        
+        pushButtonStyleSheet = """
+        
+            *{
+                background-color: rgba(0, 0, 0, 50);
+                color: white;
+            }
+        
+        """
+        
         self.scrollArea_1.setStyleSheet(sAreaStyleSheet)
         self.scrollArea_1.horizontalScrollBar().setStyleSheet(sBarStyleSheet)
         self.scrollArea_2.setStyleSheet(sAreaStyleSheet)
         self.scrollArea_2.horizontalScrollBar().setStyleSheet(sBarStyleSheet)
 
-#         
-    
-    def get_data(self):
-        self.response = requests.get('https://api.darksky.net/forecast/INSERT YOUR API HERE/50.0646,19.9450?units=si')
+        self.lineEdit.setStyleSheet(lineEditStyleSheet)
+        self.pushButton.setStyleSheet(pushButtonStyleSheet)
 
+
+    def get_cities(self):
+        self.cities = {}
+        wb = xl.load_workbook('worldcities.xlsx')
+        wb = wb.get_sheet_by_name('Sheet1')
+        for i in range(2, wb.max_row + 1):
+            self.cities[wb.cell(row = i, column = 1).value + ', ' + wb.cell(row = i, column = 5).value] = [wb.cell(row = i, column = 3).value, wb.cell(row = i, column = 4).value]
+            
+
+        
+    def autocomplete(self):
+        completer = QtWidgets.QCompleter()
+        completer.setMaxVisibleItems(10)
+        self.lineEdit.setCompleter(completer)
+         
+        model = QtCore.QStringListModel()
+        model.setStringList(self.cities)
+        completer.setModel(model)
+      
+    def get_data(self):
+        try:
+            city = self.cities[self.lineEdit.text()]
+            lon = str(city[0])
+            lat = str(city[1])
+            self.response = requests.get('https://api.darksky.net/forecast/YOUR API KEY/' + lon + ',' + lat + '?units=si')
+        except:
+            self.response = requests.get('https://api.darksky.net/forecast/YOUR API KEY/50.0646,19.9450?units=si')
+            
     def change_font(self):
         lab = MainWindow.findChildren(QtWidgets.QLabel)
-        for i in lab:
-            i.setFont(QtGui.QFont('SansSerif', 16))
+        for i in lab[-14:]:
+            i.setFont(QtGui.QFont('SansSerif', 14))
         
     def winddir_rotation(self, x):
         im = Image.open('icons\\winddir.png')
@@ -223,28 +284,28 @@ class Ui_MainWindow(object):
 #    0 FOR CURRENT, 1 FOR HOURLY, 2 FOR DAILY 
     def value_handler(self, x):
         self.values()
-        self.temp.setText(str(round(self.temps[x], 1)) + ' °C')
-        self.cloud.setText(str(round(self.clouds[x] * 100)) + ' %')
-        self.humidity.setText(str(round(self.humiditys[x] * 100)) + ' %')
-        self.pressure.setText(str(round(self.pressures[x],1)) + ' hPa')
-        self.wind.setText(str(round((self.winds[x] * 3600) / 1000)) + ' km/h')
-        self.rain.setText(str(round(self.rainchances[x] * 100, 1)) + ' %')
+        self.temp.setText('Temperature\n' + str(round(self.temps[x], 1)) + ' °C')
+        self.cloud.setText('Cloud Cover\n' + str(round(self.clouds[x] * 100)) + ' %')
+        self.humidity.setText('Humidity\n' + str(round(self.humiditys[x] * 100)) + ' %')
+        self.pressure.setText('Pressure\n' + str(round(self.pressures[x],1)) + ' hPa')
+        self.wind.setText('Wind Speed\n' + str(round((self.winds[x] * 3600) / 1000)) + ' km/h')
+        self.rain.setText('Rain Probability\n' + str(round(self.rainchances[x] * 100, 1)) + ' %')
         if 337.5 <= self.winddirs[x] < 22.5:
-            self.winddir.setText(str('N'))
+            self.winddir.setText('Wind Direction\n' + str('N'))
         if 22.5 <= self.winddirs[x] < 67.5:
-            self.winddir.setText(str('NE'))
+            self.winddir.setText('Wind Direction\n' + str('NE'))
         if 67.5 <= self.winddirs[x] < 112.5:
-            self.winddir.setText(str('E'))
+            self.winddir.setText('Wind Direction\n' + str('E'))
         if 112.5 <= self.winddirs[x] < 157.5:
-            self.winddir.setText(str('SE'))
+            self.winddir.setText('Wind Direction\n' + str('SE'))
         if 157.5 <= self.winddirs[x] < 202.5:
-            self.winddir.setText(str('S'))
+            self.winddir.setText('Wind Direction\n' + str('S'))
         if 202.5 <= self.winddirs[x] < 247.5:
-            self.winddir.setText(str('SW'))
+            self.winddir.setText('Wind Direction\n' + str('SW'))
         if 247 <= self.winddirs[x] < 292.5:
-            self.winddir.setText(str('W'))
+            self.winddir.setText('Wind Direction\n' + str('W'))
         if 292.5 <= self.winddirs[x] < 337.5:
-            self.winddir.setText(str('NW'))
+            self.winddir.setText('Wind Direction\n' + str('NW'))
             
     def values(self):
         self.htime = []
@@ -294,9 +355,9 @@ class Ui_MainWindow(object):
         
 #         DAILY DATA
         for i in range(len(self.vals['daily']['data'])):
-            self.dtime.append(self.vals['hourly']['data'][i]['time'])
-            self.dtemps.append(self.vals['hourly']['data'][i]['temperature'])
-            self.dclouds.append(self.vals['hourly']['data'][i]['cloudCover'],) 
+            self.dtime.append(self.vals['daily']['data'][i]['time'])
+            self.dtemps.append(self.vals['daily']['data'][i]['temperatureMax'])
+            self.dclouds.append(self.vals['daily']['data'][i]['cloudCover'],) 
         self.clouds.append(self.dayvals['cloudCover'])
         self.humiditys.append(self.dayvals['humidity'])
         self.pressures.append(self.dayvals['pressure'])
@@ -307,48 +368,48 @@ class Ui_MainWindow(object):
         
     
     def hForecast(self):
-        hForecastLabels = []
+        self.hForecastLabels = []
 
         for i in range(len(self.htemps) - 1):
             
             t = time.localtime(self.htime[i]).tm_hour
             
-            hForecastLabels.append([
+            self.hForecastLabels.append([
                 QtWidgets.QLabel(self.centralwidget, text = str(t) + ':00'), 
                 QtWidgets.QLabel(self.centralwidget),
                 QtWidgets.QLabel(self.centralwidget, text = str(round(self.htemps[i])) + ' °C')
                 ])
             
-            self.setimg(hForecastLabels[i][1], self.cloud_icon_handler(self.hclouds, i))
+            self.setimg(self.hForecastLabels[i][1], self.cloud_icon_handler(self.hclouds, i))
             
-            self.scrollArea_1_layout.addWidget(hForecastLabels[i][0], 0, i)
-            self.scrollArea_1_layout.addWidget(hForecastLabels[i][1], 1, i)
-            self.scrollArea_1_layout.addWidget(hForecastLabels[i][2], 2, i)
+            self.scrollArea_1_layout.addWidget(self.hForecastLabels[i][0], 0, i)
+            self.scrollArea_1_layout.addWidget(self.hForecastLabels[i][1], 1, i)
+            self.scrollArea_1_layout.addWidget(self.hForecastLabels[i][2], 2, i)
             
             for j in range(3):
-                hForecastLabels[i][j].setAlignment(QtCore.Qt.AlignCenter)
+                self.hForecastLabels[i][j].setAlignment(QtCore.Qt.AlignCenter)
 
     def dForecast(self):
-        dForecastLabels = []
+        self.dForecastLabels = []
         
         for i in range(len(self.dtemps) - 1):
             
             t = time.localtime(self.htime[i]).tm_hour
             
-            dForecastLabels.append([
+            self.dForecastLabels.append([
                 QtWidgets.QLabel(self.centralwidget, text = self.weekday_handler()[i]), 
                 QtWidgets.QLabel(self.centralwidget),
                 QtWidgets.QLabel(self.centralwidget, text = str(round(self.dtemps[i])) + ' °C')
                 ])
             
-            self.setimg(dForecastLabels[i][1], self.cloud_icon_handler(self.dclouds, i))
+            self.setimg(self.dForecastLabels[i][1], self.cloud_icon_handler(self.dclouds, i))
             
-            self.scrollArea_2_layout.addWidget(dForecastLabels[i][0], 0, i)
-            self.scrollArea_2_layout.addWidget(dForecastLabels[i][1], 1, i)
-            self.scrollArea_2_layout.addWidget(dForecastLabels[i][2], 2, i)
+            self.scrollArea_2_layout.addWidget(self.dForecastLabels[i][0], 0, i)
+            self.scrollArea_2_layout.addWidget(self.dForecastLabels[i][1], 1, i)
+            self.scrollArea_2_layout.addWidget(self.dForecastLabels[i][2], 2, i)
             
             for j in range(3):
-                dForecastLabels[i][j].setAlignment(QtCore.Qt.AlignCenter)
+                self.dForecastLabels[i][j].setAlignment(QtCore.Qt.AlignCenter)
 
     def weekday_handler(self):
         var = []
@@ -385,23 +446,43 @@ class Ui_MainWindow(object):
         if var[x] > .7:
             return 'cloud'
         
+    def rm_widgets(self):
+        d = self.dForecastLabels
+        h = self.hForecastLabels
+        for i in range(len(d)):
+            for j in range(len(d[i])):
+                d[i][j].hide()
+        for i in range(len(h)):
+            for j in range(len(h[i])):
+                h[i][j].hide()
+        
     def refresh(self):
 #         while True:
             self.value_handler(0)
+            try:
+                self.rm_widgets()
+            except:
+                pass
             self.icon_handler(0)
+            self.hForecast()
+            self.dForecast()
 #             time.sleep(10)
         
-    
+        
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
-    ui.set_background()
+    MainWindow.show()
+    thread1 = threading.Thread(target = ui.get_cities)
+    thread1.start()
+    thread1.join()
+    thread2 = threading.Thread(target = ui.autocomplete())
+    thread2.start()
+    ui.set_misc()
     ui.refresh()
     ui.change_font()
-    ui.hForecast()
-    ui.dForecast()
-    MainWindow.show()
     sys.exit(app.exec_())
+
